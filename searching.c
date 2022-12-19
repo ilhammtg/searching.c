ff07345325edb534c08865f5d49bdaeb66be8b50
ilhammtg <97074148+ilhammtg@users.noreply.github.com>
2022-12-19 14:04:24 +0700

    binery searching
    binery searching bahasa c

--- searching.c
+++ searching.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
- 
-int linearSearch(int data[],int datayangdicari){
-  
-  //looping untuk mencari datanya
-  int bykdata;
-    for(int i= 0; i<bykdata;i++){       
-      if(data[i]==datayangdicari)return i;
-    }
- 
-  return -1;  
+
+int binarySearch(int array[], int CariAngka, int left, int right) {
+  if (right >= left) {
+    int mid = left + (right - left) / 2;
+
+    // Jika menemukan angka tengah maka return mid
+    if (array[mid] == CariAngka)
+      return mid;
+
+    // Cari angka pada bagian kiri
+    if (array[mid] > CariAngka)
+      return binarySearch(array, CariAngka, left, mid - 1);
+
+    // Cari angka pada bagian kanan
+    return binarySearch(array, CariAngka, mid + 1, right);
+  }
+
+  return -1;
 }
- 
-int main (){
-  int data[4]={9,11,1,8}; //data
-  int indexSearch = linearSearch(data,1); //(data, angka yang dicari)
- 
-  if(indexSearch == -1){
-  printf("Data tidak ditemukan dalam array\n");
+
+int main() {
+  int array[] = {7, 11, 12, 23, 29, 32, 38};
+  int n = sizeof(array) / sizeof(array[0]);
+  int CariAngka = 32; //angka yang ingin dicari
+  int hasil = binarySearch(array, CariAngka, 0, n - 1);
+
+  if (hasil == -1){
+    printf("Data tidak ditemukan\n");
   }
   else{
-  printf("Data %d ditemukan dalam array\n",data[indexSearch]);  
+    printf("Angka %d ditemukan pada index %d\n", CariAngka, hasil);
   }
+
   return 0;
+}
